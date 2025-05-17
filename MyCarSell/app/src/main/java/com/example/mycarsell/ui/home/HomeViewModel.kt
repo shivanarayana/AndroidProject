2ec20ff1605a90eb7642ceb34b9b898012f26844
shiva modini <shhivaa00@gmail.com>
2025-05-17 18:32:06 +0530

    trying to add the compose here

--- MyCarSell/app/src/main/java/com/example/mycarsell/ui/home/HomeViewModel.kt
+++ MyCarSell/app/src/main/java/com/example/mycarsell/ui/home/HomeViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.mycarsell.ui.home
 
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -10,4 +11,24 @@ class HomeViewModel : ViewModel() {
         value = "This is a Car Shopping app"
     }
     val text: LiveData<String> = _text
+
+    // --- Header Data ---
+    private val _headerTitle = MutableLiveData<String>().apply {
+        value = "CarSellMax!" // Example header title
+    }
+    val headerTitle: LiveData<String> = _headerTitle
+
+    private val _headerColor = MutableLiveData<androidx.compose.ui.graphics.Color>().apply {
+        value = Color.Red // Set the header color to Red
+    }
+    val headerColor: LiveData<androidx.compose.ui.graphics.Color> = _headerColor
+
+    // --- Example functions to update header data (optional) ---
+    fun updateHeaderText(newTitle: String) {
+        _headerTitle.value = newTitle
+    }
+
+    fun updateHeaderColor(newColor: androidx.compose.ui.graphics.Color) {
+        _headerColor.value = newColor
+    }
 }
